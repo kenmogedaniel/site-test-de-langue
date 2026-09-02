4045ce222e42b8714d7ffadc0903743489e0b1f7
Takou237 <jordankenmogne28@gmail.com>
2026-09-02 05:45:51 +0100

    fix: allow flagcdn.com in content security policy
    The CSP img-src directive was blocking all flag images from flagcdn.com, breaking flags on every language page. Added the domain to img-src.

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const securityHeaders = [
       "default-src 'self'; " +
       "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
       "style-src 'self' 'unsafe-inline'; " +
-      "img-src 'self' data: blob:; " +
+      "img-src 'self' data: blob: https://flagcdn.com; " +
       "font-src 'self' data:; " +
       "connect-src 'self' https://*.supabase.co wss://*.supabase.co; " +
       "media-src 'self' blob: data:; " +
